6ee74a3d1fcf8a6523500a63cb332aa43894ae79
Joao Oliveira <joaooliveira1775@gmail.com>
2019-12-20 12:27:42 -0300

    second module done

---
+++ exercicios-modulo-02/exercicio.js
@@ -0,0 +1,12 @@
+//1.1
+import ClasseUsuario from './function';
+
+ClasseUsuario.info();
+
+//1.2
+
+import { idade } from './function';
+console.log(idade);
+
+//1.3
+import defaultUsuario, { idade as IdadeUsuario } from './function';
