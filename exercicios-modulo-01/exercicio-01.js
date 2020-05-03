c6ab02def959859a0148fc2ea97d036991d9eae8
adrielbolandini <64433279+adrielbolandini@users.noreply.github.com>
2020-05-02 22:11:18 -0300

    Update exercicio-01.js
    Modificação para que a classe filha Admin passe corretamente o email e a senha para a classe pai usuario.

--- exercicios-modulo-01/exercicio-01.js
+++ exercicios-modulo-01/exercicio-01.js
@@ -8,12 +8,23 @@ class Usuario {
     }
 
 }
+
+/* Sua contrução está equivocada aqui, bati muito a cabeça nessa parte, mas basicamente, se você der um console.log no Adm1,
+ele retornará undefined,undefined,true. Ou seja, o Admin não está passando o email e a senha para o usuario, isso acontece 
+por causa do constructor na classe admin sobrepondo o construtor da classe Usuario.
+
 class Admin extends Usuario {
     constructor() {
         super();
         this.admin = true;
     }
 }
+*/
+
+class Admin extends Usuario {
+        admin = true;
+}
+//Em cima a propriedade admin apenas será adicionada ao objeto resultante, mas lembre de instalar o classproperties pra isso.
 
 const User1 = new Usuario('email@teste.com', 'senha123');
 const Adm1 = new Admin('email@teste.com', 'senha123');
